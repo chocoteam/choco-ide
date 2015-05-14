32240a1fd64c3c2d2274c926bd77bef1f97665b6
Guillaume Pouilloux <guillaume.pouilloux@etudiant.mines-nantes.fr>
2015-05-14 18:02:46 +0200

    Appel du service "compile" et utilisation de la réponse JSON

--- public/javascripts/chocoide.js
+++ public/javascripts/chocoide.js
@@ -5,5 +5,61 @@ window.onload = function() {
 }
 
 function compile() {
-    window.alert("Button clicked!")
+    var editor = ace.edit("editor");
+    var code = editor.getSession().getValue();
+
+    // Fire the HTTP POST request
+    request = $.ajax({
+        url: "/compile",
+        type: "post",
+        data: { body: code }
+    });
+
+    // Callback handler that will be called on success - HTTP 200 OK
+    request.done(function (response, textStatus, jqXHR){
+
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // bouchonnage / code dans request.fail en attendant que le service soit développé
+        // a déplacer dans request.success
+        var response = '{"Errors":"Duplicate import of package fr.toto","Events":[{"Message":"Hello guys","Kind":"stdout","Delay":0},{"Message":"2009/11/10 23:00:00 Error..","Kind":"stderr","Delay":0}]}';
+
+        var jsonObject = jQuery.parseJSON(response);
+        var compilationMessage = jsonObject.Errors;
+        var runtimeEvents = jsonObject.Events;
+
+        var console = document.getElementById("console");
+
+        if(compilationMessage != "") {
+            console.innerHTML += "<p style=\"color:red; background-color: black\">Error during compilation : " + compilationMessage + "</p>";
+        }
+
+        runtimeEvents.forEach(function(runtimeEvent) {
+            var textColor = "blue";
+            if(runtimeEvent.Kind == "stdout") {
+
+            }
+            else if(runtimeEvent.Kind == "stderr") {
+                textColor = "red"
+            }
+            else {
+                // ????
+            }
+            console.innerHTML += "<p style=\"color:"+textColor+";\">" + runtimeEvent.Message + "</p>";
+        });
+
+        //////////////////////////////////////
+
+        // Log the error to the console
+        //console.error(
+        //    "The following error occurred: "+
+        //    textStatus, errorThrown
+        //);
+    });
+
+    // Callback handler that will always be called
+    request.always(function () {
+    });
 }
