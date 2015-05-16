c09e5258efbad29542a89b60b644acafc0848d85
TidyMaze <rolland_yann@orange.fr>
2015-05-16 12:08:37 +0200

    fixed Line endings (CRLF => LF), added call to compiler

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import compilation.StringCompilerAndRunner;
+import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
-import static play.mvc.Results.ok;
+import java.util.Map;
 
-public class Application {
+public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Let's get working guys !"));
@@ -17,6 +19,21 @@ public class Application {
      * @return result compilation result
      */
     public static Result compile() {
+        System.out.println(request().body());
+        final Map<String, String[]> mapParameters = request().body().asFormUrlEncoded();
+        String code = mapParameters.get("body")[0];
+        System.out.println(code);
+        //compileAndRunFromString(code);
         return ok("compilation OK");
+    }
+
+    private static void compileAndRunFromString() {
+        StringCompilerAndRunner compilerAndRunner = new StringCompilerAndRunner();
+        String code = "code";
+        try {
+            compilerAndRunner.compileAndRun(code);
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
