423075fd147ac674666edb219785bfac68dbe11f
TidyMaze <rolland_yann@orange.fr>
2015-06-04 13:43:11 +0200

    Suppression ProcessRunner (inclu dans ProcessStrategy), gestion des classes avec un nom différent de Main

--- app/compilation/ProcessStrategy.java
+++ app/compilation/ProcessStrategy.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
 /**
  * Created by yann on 04/06/15.
@@ -13,20 +15,25 @@ public abstract class ProcessStrategy {
     protected final CompilationAndRunResult compilationAndRunResult;
     protected final String sOut;
     protected final String sErr;
+    private final HashMap<RunEvent.Kind, BufferedReader> mapReaders;
 
     public ProcessStrategy(String command, CompilationAndRunResult compilationAndRunResult) throws IOException {
         this.compilationAndRunResult = compilationAndRunResult;
-        ProcessRunner processRunner = new ProcessRunner(command);
-        processRunner.blockingRun();
+        Process p = Runtime.getRuntime().exec(command);
+        BufferedReader stdInput = new BufferedReader((new InputStreamReader(p.getInputStream())));
+        BufferedReader stdError = new BufferedReader((new InputStreamReader(p.getErrorStream())));
+        mapReaders = new HashMap<>();
+        mapReaders.put(RunEvent.Kind.OUT, stdInput);
+        mapReaders.put(RunEvent.Kind.ERR, stdError);
 
-        sOut = getAndOutput(processRunner, RunEvent.Kind.OUT);
-        sErr = getAndOutput(processRunner, RunEvent.Kind.ERR);
+        sOut = getAndOutput(mapReaders, RunEvent.Kind.OUT);
+        sErr = getAndOutput(mapReaders, RunEvent.Kind.ERR);
     }
 
     @NotNull
-    private String getAndOutput(ProcessRunner processRunner, RunEvent.Kind kind) throws IOException {
+    private String getAndOutput(HashMap<RunEvent.Kind, BufferedReader> readerMap, RunEvent.Kind kind) throws IOException {
         System.out.println(kind + ":");
-        String res = stringOfReader(processRunner.getOutput(kind));
+        String res = stringOfReader(readerMap.get(kind));
         System.out.println("\""+res+"\"");
         return res;
     }
--- app/compilation/StringCompilerAndRunner.java
+++ app/compilation/StringCompilerAndRunner.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,9 +16,9 @@ import java.util.regex.Pattern;
 public class StringCompilerAndRunner {
 
     // Commandes utilisées à la compilation
-    private static final String MAIN_FILE = "ctmp/src/Main.java";
-    private static final String CALL_JAVA_MAIN = "java -cp ctmp/bin/:lib/* Main";
-    private static final String CALL_JAVAC_MAIN = "javac -d ctmp/bin/ -cp lib/* ctmp/src/Main.java";
+    private static final String MAIN_FILE = "ctmp/src/%s.java";
+    private static final String CALL_JAVA_MAIN = "java -cp ctmp/bin/:lib/* %s";
+    private static final String CALL_JAVAC_MAIN = "javac -d ctmp/bin/ -cp lib/* ctmp/src/%s.java";
 
     // Regex permettant de trouver le nom de la classe possédant la méthode main (dans le 1er group)
     private static final String PATTERN_MAIN = "public class (\\w*)\\s\\{[\\n|\\s]*\\s*public static void main";
@@ -27,19 +28,19 @@ public class StringCompilerAndRunner {
     public CompilationAndRunResult compileAndRun(String code) throws IOException {
         System.out.println("Debut compileAndRun");
 
-        findMainClass(code);
+        String className = findMainClass(code).orElse("Main");
 
         compilationAndRunResult = new CompilationAndRunResult();
 
-        createFilesBeforeCompile(code);
-        compileCode(code, compilationAndRunResult);
+        createFilesBeforeCompile(code, className);
+        compileCode(code, compilationAndRunResult, className);
         List<RunEvent> runEvents = new ArrayList<RunEvent>();
 
         if(canRunCode()) {
             EventsRecorder eventsRecorder = new EventsRecorder();
             EventsRecorder recorder = eventsRecorder;
             try {
-                runCode(compilationAndRunResult);
+                runCode(compilationAndRunResult, className);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -49,28 +50,34 @@ public class StringCompilerAndRunner {
         return compilationAndRunResult;
     }
 
-    private void findMainClass(String code) {
+    private Optional<String> findMainClass(String code) {
         Pattern pattern = Pattern.compile(PATTERN_MAIN);
         Matcher matcher = pattern.matcher(code);
         while(matcher.find()){
-            System.out.println("main class : \"" + matcher.group(1) + "\"");
+            String name = matcher.group(1);
+            System.out.println("main class : \"" + name + "\"");
+            return Optional.of(name);
         }
+
+        return Optional.empty();
     }
 
     private boolean canRunCode() {
         return this.compilationAndRunResult.getErrors().isEmpty();
     }
 
-    private void compileCode(String code, CompilationAndRunResult compilationAndRunResult) throws IOException {
-        new CompileStrategy(CALL_JAVAC_MAIN, compilationAndRunResult).handleOutputs();
+    private void compileCode(String code, CompilationAndRunResult compilationAndRunResult, String className) throws IOException {
+        String commande = String.format(CALL_JAVAC_MAIN, className);
+        new CompileStrategy(commande, compilationAndRunResult).handleOutputs();
     }
 
-    private void runCode(CompilationAndRunResult compilationAndRunResult) throws IOException {
-        new RunStrategy(CALL_JAVA_MAIN, compilationAndRunResult).handleOutputs();
+    private void runCode(CompilationAndRunResult compilationAndRunResult, String className) throws IOException {
+        String commande = String.format(CALL_JAVA_MAIN, className);
+        new RunStrategy(commande, compilationAndRunResult).handleOutputs();
     }
 
-    private void createFilesBeforeCompile(String code) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter(MAIN_FILE, "UTF-8");
+    private void createFilesBeforeCompile(String code, String className) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(String.format(MAIN_FILE, className), "UTF-8");
         writer.println(code);
         writer.close();
     }
