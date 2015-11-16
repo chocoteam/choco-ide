3d1ef8ae80f77210af65d189fa7bbe6219fcd2ef
cprudhom <cprudhom@mines-nantes.fr>
2015-11-16 14:34:31 +0100

    few changes

--- app/datas/compilation/RunStrategy.java
+++ app/datas/compilation/RunStrategy.java
@@ -19,7 +19,7 @@ public class RunStrategy extends ProcessStrategy {
      * $3 : classname
      */
     private static final String CALL_JAVA_MAIN = "java -Djava.security.manager -Djava.security.policy=="+ Play.application().configuration().getString("security.manager.path")
-            +" -cp %1$s/bin/"+ File.pathSeparator + "%2$s -Dlogback.configurationFile=lib/logback.xml %3$s";
+            +" -cp %1$s/bin/"+ File.pathSeparator + "%2$s %3$s";
 
     public RunStrategy(CompilationAndRunResult compilationAndRunResult, Path tempDirectory, String libpath, String className, int timeout) throws IOException {
         super(String.format(CALL_JAVA_MAIN, tempDirectory.toString(), libpath, className),compilationAndRunResult, timeout);
